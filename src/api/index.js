f0f1b6f544492e4d3c9dcf31443d39b4798fff28
kakarot98 <hrushikeshmohanty93@gmail.com>
2020-07-25 20:38:28 +0530

    added counting  animation

--- src/api/index.js
+++ src/api/index.js
@@ -7,13 +7,13 @@ export const fetchData = async () => {
   //but the shortcut is to destructure the api while fetching and return only the needed parts of the api
   try {
     const {
-      data: { confirmed, recovered, deaths, lasUpdate },
+      data: { confirmed, recovered, deaths, lastUpdate },
     } = await axios.get(url); //destructuring in the same line
 
     //const modifiedData = { confirmed, recovered, deaths, lasUpdate }
     //above line is not needed since we will anyway return modifiedData but we could just return the whole raw data itself
 
-    return { confirmed, recovered, deaths, lasUpdate };
+    return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {
     console.log("error occured" + error);
   }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
+import CountUp from "react-countup";
 
-const Cards = (data) => {
-  console.log(data);
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  console.log({ confirmed, recovered, deaths, lastUpdate });
+
+  if (!confirmed) {
+    return "Loading...";
+  }
 
   return (
     <div className={styles.container}>
@@ -13,8 +18,17 @@ const Cards = (data) => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.0}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant="body2">
               No of active cases of covid19
             </Typography>
@@ -25,8 +39,17 @@ const Cards = (data) => {
             <Typography color="textSecondary" gutterBottom>
               Recovered
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={recovered.value}
+                duration={2.0}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant="body2">
               No of recoveries from covid19
             </Typography>
@@ -37,8 +60,17 @@ const Cards = (data) => {
             <Typography color="textSecondary" gutterBottom>
               Deaths
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={2.0}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant="body2">No of deaths from covid19</Typography>
           </CardContent>
         </Grid>
